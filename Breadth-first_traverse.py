e610d25d16369f0cc2b05e7f54c449946b59108d
Jaehong Park <jhpark555@gmail.com>
2024-05-07 10:46:43 -0700

    dfs bug fix

--- Breadth-first_traverse.py
+++ Breadth-first_traverse.py
@@ -27,4 +27,31 @@ def breadth_first_search(graph,root):
                 graph_queue.append(elem)
                 print(' ',elem)
 
-breadth_first_search(graph,'A')
+def depth_first_search(graph,root):
+    visited_vertices= list()
+    graph_stack = list()
+
+    graph_stack.append(root)
+    node = root
+    while len(graph_stack) >0:
+        if node not in visited_vertices:
+            visited_vertices.append(node)
+        adj_nodes=graph[node]
+
+        if set(adj_nodes).issubset( set(visited_vertices)):
+            graph_stack.pop()
+            if len(graph_stack) >0:
+                node = graph_stack[-1]
+                continue
+        else:
+            remaining_elements=set(adj_nodes).difference(set(visited_vertices))
+            first_adj_node =sorted(remaining_elements)[0]
+            graph_stack.append(first_adj_node)
+            node=first_adj_node
+            #print(visited_vertices)
+    return visited_vertices
+
+
+#breadth_first_search(graph,'A')
+x=depth_first_search(graph,'A')
+print(x)
